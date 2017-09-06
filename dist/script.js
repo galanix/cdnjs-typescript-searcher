2abff7c08e05d87039e85facec4a7136d7dff29e
Vadym Hrynishyn <vadym.galanix@gmail.com>
2017-09-06 18:56:56 +0300

    Add functionality (doesn't work correctly)

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,8 @@
 var myRequest = new XMLHttpRequest();
 var myData;
-myRequest.open('GET', 'https://api.cdnjs.com/libraries');
+var url = "https://api.cdnjs.com/libraries";
+var placeForData = document.getElementById("dataHolder");
+myRequest.open('GET', url);
 myRequest.onload = function () {
     myData = JSON.parse(myRequest.responseText);
 };
@@ -32,11 +34,14 @@ function sendForFiltering() {
 function showFilteredData(data, ifShow, isEmptyString) {
     if (ifShow === void 0) { ifShow = true; }
     if (isEmptyString === void 0) { isEmptyString = false; }
-    var placeForData = document.getElementById("dataHolder");
     if (ifShow === true) {
         placeForData.innerHTML = "\n\t\t<tr>\n\t\t\t<th>Name</th>\n\t\t\t<th>Link</th>\n\t\t</tr>";
+        var btnNum = 0;
+        var btnId;
         for (var i = 0; i < data.length; i++) {
-            placeForData.innerHTML += "\n\t\t\t\t<tr>\n\t\t\t\t\t<td>" + data[i].name + "</td>\n\t\t\t\t\t<td>" + data[i].latest + "</td>\n\t\t\t\t</tr>\n\t\t\t";
+            btnNum += 1;
+            btnId = "btn" + btnNum;
+            placeForData.innerHTML += "\n\t\t\t\t<tr>\n\t\t\t\t\t<td><button id=\"" + btnId + "\" onclick=\"redirectToNewPage(this.id)\">" + data[i].name + "</button></td>\n\t\t\t\t\t<td>" + data[i].latest + "</td>\n\t\t\t\t</tr>\n\t\t\t";
         }
     }
     else {
@@ -46,6 +51,49 @@ function showFilteredData(data, ifShow, isEmptyString) {
         }
         else if (isEmptyString === false) {
             placeForData.innerHTML = "Please, write MORE than just one letter";
+        }
+    }
+}
+function getDataFromNewRequest(param) {
+    var newUrl = url + "/" + param;
+    var newRequest = new XMLHttpRequest();
+    newRequest.open('GET', newUrl);
+    newRequest.onload = function () {
+        sendInfoToSessionStorage(param, JSON.parse(newRequest.responseText));
+    };
+    newRequest.send();
+}
+function sendInfoToSessionStorage(param, data) {
+    sessionStorage.setItem('parameter', param);
+    sessionStorage.setItem('data', JSON.stringify(data));
+    console.log(param, data);
+}
+function redirectToNewPage(id) {
+    var button = document.getElementById("" + id);
+    var param = button.innerText;
+    var w = window.open("newpage.html#" + param);
+}
+function workOnNewPage() {
+    var myUrl = window.location.href;
+    var urlArray = myUrl.split('#');
+    var parameter = urlArray[1];
+    getDataFromNewRequest(parameter);
+    fillNewPage();
+}
+function fillNewPage() {
+    setPageContent();
+}
+function setPageContent() {
+    var data = JSON.parse(sessionStorage.getItem("data"));
+    var placeForData = document.getElementById("newDataHolder");
+    placeForData.innerHTML = "\n\t\t<ul>\n\t\t\t<li><b>Name:</b> " + data.name + "</li>\n\t\t\t<li><b>Description:</b> " + data.description + "</li>\n\t\t\t<li><b>Author:</b> " + data.author.name + "</li>\n\t\t\t<li><b>Homepage:</b> " + data.homepage + "</li>\n\t\t\t<li><b>License:</b> " + data.license + "</li>\n\t\t\t<li><b>Keywords:</b> " + data.keywords + "</li>\n\t\t\t<li><b>Version:</b> " + data.version + "</li>\n\t\t\t<li><b>Repository:</b> " + data.repository.url + "</li>\n\t\t\t<li><b>Versions list:</b>\n\t\t\t\t<ul id=\"list\"></ul>\n\t\t\t</li>\n\t\t</ul>\n\t";
+    var placeForAssets = document.getElementById("list");
+    for (var i = 0; i < data.assets.length; i++) {
+        if (i === data.assets.length - 1) {
+            placeForAssets.innerHTML += "" + data.assets[i].version;
+        }
+        else {
+            placeForAssets.innerHTML += data.assets[i].version + ", ";
         }
     }
 }
--- script.ts
+++ script.ts
@@ -1,7 +1,11 @@
 var myRequest = new XMLHttpRequest();
 var myData: any;
+var url: string = `https://api.cdnjs.com/libraries`;
+var placeForData = <HTMLInputElement>document.getElementById("dataHolder");
 
-myRequest.open('GET', 'https://api.cdnjs.com/libraries');
+
+
+myRequest.open('GET', url);
 myRequest.onload = function () {
     myData = JSON.parse(myRequest.responseText);
 };
@@ -35,18 +39,22 @@ function sendForFiltering() {
 }
 
 function showFilteredData(data: any, ifShow: boolean = true, isEmptyString: boolean = false) {
-	var placeForData = <HTMLInputElement>document.getElementById("dataHolder");
-
 	if (ifShow === true) {
 		placeForData.innerHTML = `
 		<tr>
 			<th>Name</th>
 			<th>Link</th>
 		</tr>`;
+
+		var btnNum: number = 0;
+		var btnId: string;
+
 		for (var i = 0; i < data.length; i++) {
+			btnNum += 1;
+			btnId = `btn${btnNum}`
 			placeForData.innerHTML += `
 				<tr>
-					<td>${data[i].name}</td>
+					<td><button id="${btnId}" onclick="redirectToNewPage(this.id)">${data[i].name}</button></td>
 					<td>${data[i].latest}</td>
 				</tr>
 			`
@@ -57,6 +65,73 @@ function showFilteredData(data: any, ifShow: boolean = true, isEmptyString: bool
 			return;
 		} else if (isEmptyString === false) {
 			placeForData.innerHTML = `Please, write MORE than just one letter`;
+		}
+	}
+}
+
+function getDataFromNewRequest(param: string): any {
+	var newUrl: string = `${url}/${param}`;
+	var newRequest = new XMLHttpRequest();
+
+	newRequest.open('GET', newUrl);
+	newRequest.onload = function () {
+	    sendInfoToSessionStorage(param, JSON.parse(newRequest.responseText));
+	};
+	newRequest.send();
+}
+
+function sendInfoToSessionStorage(param: string, data: any) {
+	sessionStorage.setItem('parameter', param);
+	sessionStorage.setItem('data', JSON.stringify(data));
+	console.log(param, data);
+}
+
+function redirectToNewPage(id: string) {
+	var button = <HTMLInputElement>document.getElementById(`${id}`);
+	var param: string = button.innerText;
+
+	var w = window.open(`newpage.html#${param}`);
+}
+
+function workOnNewPage() {
+	var myUrl: string = window.location.href;
+	var urlArray = myUrl.split('#');
+	var parameter = urlArray[1];
+
+	getDataFromNewRequest(parameter);
+	fillNewPage();
+}
+
+function fillNewPage() {
+	setPageContent();
+}
+
+function setPageContent() {
+	var data: any = JSON.parse(sessionStorage.getItem("data"));
+	var placeForData = <HTMLInputElement>document.getElementById("newDataHolder");
+
+	placeForData.innerHTML = `
+		<ul>
+			<li><b>Name:</b> ${data.name}</li>
+			<li><b>Description:</b> ${data.description}</li>
+			<li><b>Author:</b> ${data.author.name}</li>
+			<li><b>Homepage:</b> ${data.homepage}</li>
+			<li><b>License:</b> ${data.license}</li>
+			<li><b>Keywords:</b> ${data.keywords}</li>
+			<li><b>Version:</b> ${data.version}</li>
+			<li><b>Repository:</b> ${data.repository.url}</li>
+			<li><b>Versions list:</b>
+				<ul id="list"></ul>
+			</li>
+		</ul>
+	`;
+
+	var placeForAssets = <HTMLInputElement>document.getElementById("list");
+	for (var i = 0; i < data.assets.length; i++) {
+		if (i === data.assets.length - 1) {
+			placeForAssets.innerHTML += `${data.assets[i].version}`;
+		} else {
+			placeForAssets.innerHTML += `${data.assets[i].version}, `;
 		}
 	}
 }
